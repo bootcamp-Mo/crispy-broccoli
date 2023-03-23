f10f1057e92877b5cbcc74891e90eb8ae807b570
Monique <mo.sherred@gmail.com>
2023-03-23 14:31:46 -0700

    badges not working in README

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# crispy-broccoli
+
+# dasklf
+
+## Description
+    gsfdg
+        
+## Table of Contents
+1. [Description](#description)
+2. [Installation](#installation)
+3. [Usage](#usage)
+4. [License](#license)
+5. [Contributing](#contributing)
+6. [Tests](#tests)
+7. [Questions](#questions)
+        
+## Installation
+    dfgsd
+        
+## Usage
+    sdfgsd
+        
+## License
+    [![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)
+        
+## Contributing
+    sdfg
+        
+## Tests
+    fsdgs
+        
+## Questions
+	If you have any questions, feel free to reach out through the following channels:
+
+- [GitHub](https://github.com/sdfgsdfg)
+- Email: sdfgsdf (please use "GitHub Project Inquiry" as the subject)
+
+    
--- index.js
+++ index.js
@@ -1,86 +1,119 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-const generateREADME =
-    ({ projectName, description, tableCont, install, usage,
-        license, contribute, tests, questions, }) => `
-        # ${projectName}
+const getLicenseBadge = (license) => {
+	const licenseBadges = {
+		'MIT': '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)',
+		'Apache 2.0': '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)',
+		'IBM PLV 1.0': '[![License: IPL 1.0](https://img.shields.io/badge/License-IPL_1.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)',
+		'Mozilla PL 2.0': '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)',
+		'None': '',
+	};
 
-        ## Description
-        ${description}
+	return licenseBadges[license];
+};
+
+const generateREADME =
+	({ projectName, description, install, usage,
+		license, contribute, tests, githubUsername, email, }) => `
+# ${projectName}
+
+## Description
+    ${description}
         
-        ## Table of Contents
-        ${tableCont}
+## Table of Contents
+1. [Description](#description)
+2. [Installation](#installation)
+3. [Usage](#usage)
+4. [License](#license)
+5. [Contributing](#contributing)
+6. [Tests](#tests)
+7. [Questions](#questions)
         
-        ## Installation
-        ${install}
+## Installation
+    ${install}
         
-        ## Usage
-        ${usage}
+## Usage
+    ${usage}
         
-        ## License
-        ${license}
+## License
+    ${getLicenseBadge(license)}
         
-        ## Contributing
-        ${contribute}
+## Contributing
+    ${contribute}
         
-        ## Tests
-        ${tests}
+## Tests
+    ${tests}
         
-        ## Questions
-        ${questions}
+## Questions
+	${generateQuestionsSection(githubUsername, email)}
     `;
+
+const generateQuestionsSection = (githubUsername, email) => {
+	return `If you have any questions, feel free to reach out through the following channels:
+
+- [GitHub](https://github.com/${githubUsername})
+- Email: ${email} (please use "GitHub Project Inquiry" as the subject)
+`;
+};
+
 inquirer
-    .prompt([
-        {
-            type: 'input',
-            name: 'projectName',
-            message: 'Project name: '
-        },
-        {
-            type: 'input',
-            name: 'description',
-            message: 'Description: '
-        },
-        {
-            type: 'input',
-            name: 'tableCont',
-            message: 'Table of Contents: '
-        },
-        {
-            type: 'input',
-            name: 'install',
-            message: 'Installation: '
-        },
-        {
-            type: 'input',
-            name: 'usage',
-            message: 'Usage: '
-        },
-        {
-            type: 'input',
-            name: 'license',
-            message: 'License: '
-        },
-        {
-            type: 'input',
-            name: 'contribute',
-            message: 'Contributing: '
-        },
-        {
-            type: 'input',
-            name: 'tests',
-            message: 'Tests: '
-        },
-        {
-            type: 'input',
-            name: 'questions',
-            message: 'Questions: '
-        },
-    ])
-    .then((data) => {
-        const readmeContent = generateREADME(data);
-        writeFile('README.md', readmeContent, (err) =>
-            err ? console.log(err) : console.log('Success!')
-        );
-    });
+	.prompt([
+		{
+			type: 'input',
+			name: 'projectName',
+			message: 'Project name: '
+		},
+		{
+			type: 'input',
+			name: 'description',
+			message: 'Description: '
+		},
+		{
+			type: 'input',
+			name: 'install',
+			message: 'Installation: '
+		},
+		{
+			type: 'input',
+			name: 'usage',
+			message: 'Usage: '
+		},
+		{
+			type: 'list',
+			name: 'license',
+			message: 'Choose a license for your application:',
+			choices: ['MIT', 'Apache 2.0', 'IBM PLV 1.0', 'Mozilla PL 2.0', 'None'],
+		},
+		{
+			type: 'input',
+			name: 'contribute',
+			message: 'Contributing: '
+		},
+		{
+			type: 'input',
+			name: 'tests',
+			message: 'Tests: '
+		},
+		{
+			type: 'input',
+			name: 'questions',
+			message: 'Questions: '
+		},
+		{
+			type: 'input',
+			name: 'githubUsername',
+			message: 'GitHub username: '
+		},
+		{
+			type: 'input',
+			name: 'email',
+			message: 'Email address: '
+		},
+	])
+	.then(async (data) => {
+		const readmeContent = generateREADME(data);
+		fs.writeFile('README.md', readmeContent, (err) =>
+			err ? console.log(err) : console.log('Success!')
+		);
+	});
