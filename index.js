84f4294bd5516935d95179d0289cdb8530ce4164
Monique <mo.sherred@gmail.com>
2023-03-23 14:42:24 -0700

    added comments

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
+// This function returns a badge image based on the input license. 
+// It uses an object to map licenses to their respective badges.
 const getLicenseBadge = (license) => {
 	const licenseBadges = {
 		'MIT': '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)',
@@ -12,7 +14,8 @@ const getLicenseBadge = (license) => {
 
 	return licenseBadges[license];
 };
-
+// This function generates the README content using a template string. 
+// It takes an object with properties for each section and returns a formatted string.
 const generateREADME =
 	({ projectName, description, install, usage,
 		license, contribute, tests, githubUsername, email, }) => `
@@ -49,6 +52,8 @@ const generateREADME =
 	${generateQuestionsSection(githubUsername, email)}
     `;
 
+// This function generates the Questions section of the README 
+// using the provided GitHub username and email address.
 const generateQuestionsSection = (githubUsername, email) => {
 	return `If you have any questions, feel free to reach out through the following channels:
 
@@ -57,6 +62,9 @@ const generateQuestionsSection = (githubUsername, email) => {
 `;
 };
 
+// This part of the code prompts the user for input using an array of question objects. 
+// Each object defines a question, its type, and the name of the property that will store the answer. 
+// After getting the user input, the code executes a callback function with the collected data.
 inquirer
 	.prompt([
 		{
@@ -112,6 +120,10 @@ inquirer
 		},
 	])
 	.then(async (data) => {
+		// This is a callback function, the code calls generateREADME to generate the README 
+		// content based on the user's input. Then, it writes the content to a README.md 
+		// file using the fs.writeFile function. If there's an error, it prints the error; 
+		// otherwise, it prints a success message.
 		const readmeContent = generateREADME(data);
 		fs.writeFile('README.md', readmeContent, (err) =>
 			err ? console.log(err) : console.log('Success!')
